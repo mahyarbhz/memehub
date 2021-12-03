d8a3fd0f6f58cf068f96b957ebc02cf001580931
Mahyar Behzadi <behzadimahyar85@gmail.com>
2021-12-03 10:07:30 +0330

    Delete table
    + update styles

--- resources/views/livewire/category/index.blade.php
+++ resources/views/livewire/category/index.blade.php
@@ -23,57 +23,62 @@
             </div>
         </form>
     </div>
+    <style>
+        ul {
+            width: 400px;
+            text-align: center;
+            margin-top: 1rem;
+        }
+
+        ul .head {
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: .5rem;
+        }
+
+        ul .head p {
+            font-weight: bold;
+            width: 100%;
+        }
+
+        ul .item {
+            display: flex;
+            justify-content: space-between;
+            border: 2px solid #32393d;
+            padding: .5rem;
+            text-align: center;
+        }
+
+        ul .item p,form {
+            width: 100%;
+        }
+
+        ul .item p.item-handle:hover {
+            cursor: row-resize;
+        }
+    </style>
     <div class="flex justify-center mt-8">
         Categories
     </div>
     <div class="flex justify-center mt-4">
-{{--        <table wire:sortable="updateCategoryOrder" class="border-collapse table-auto border border-green-800 w-full">--}}
-{{--            <thead class="bg-blue-200">--}}
-{{--            <tr>--}}
-{{--                <th class="border border-blue-600 px-2 py-2">Id</th>--}}
-{{--                <th class="border border-blue-600 px-2 py-2">Name</th>--}}
-{{--                <th class="border border-blue-600 px-2 py-2">Position</th>--}}
-{{--                <th class="border border-blue-600 px-2 py-2">Action</th>--}}
-{{--            </tr>--}}
-{{--            </thead>--}}
-{{--            <tbody>--}}
-{{--            @foreach($categories as $category)--}}
-{{--                <tr wire:sortable.item="{{ $category->id }}" wire:key="task-{{ $category->id }}">--}}
-{{--                    <td class="border border-blue-600 py-2 pl-2">{{ $category->id }}</td>--}}
-{{--                    <td class="border border-blue-600 py-2 pl-2">{{ $category->name }}</td>--}}
-{{--                    <td wire:sortable.handle class="border border-blue-600 py-2 pl-2">{{ $category->position }}</td>--}}
-{{--                    <td class="border border-blue-600 py-2 pl-2">--}}
-{{--                        <form action="{{ route("categories.destroy", $category->id) }}" method="post">--}}
-{{--                            <a href="{{ route('categories.edit', $category->id) }}" class="mx-1 px-1 py-1 rounded"><i class="text-yellow-500 fas fa-edit"></i></a>--}}
-{{--                            @csrf--}}
-{{--                            @method('DELETE')--}}
-{{--                            <button class="mx-1 px-1 py-1 rounded outline-none" type="submit"><i class="text-red-500 fas fa-trash"></i></button>--}}
-{{--                        </form>--}}
-{{--                    </td>--}}
-{{--                </tr>--}}
-{{--            @endforeach--}}
-{{--            </tbody>--}}
-{{--        </table>--}}
-        <ul wire:sortable="updateCategoryOrder" style="width: 400px; text-align: center; margin-top: 1rem">
-            <li style="display: flex; justify-content: space-between">
-                <p style="width: 100%">Name</p>
-                <p style="width: 100%">Position</p>
-                <p style="width: 100%">Handle</p>
-                <p style="width: 100%">Action</p>
+        <ul wire:sortable="updateCategoryOrder">
+            <li class="head">
+                <p>Name</p>
+                <p>Position</p>
+                <p>Handle</p>
+                <p>Action</p>
             </li>
             @foreach ($categories as $category)
-                <li wire:sortable.item="{{ $category->id }}" wire:key="task-{{ $category->id }}" style="display: flex; justify-content: space-between; border: 2px solid #32393d; padding: 8px; text-align: center;">
-                    <p style="width: 100%">{{ $category->name }}</p>
-                    <p style="width: 100%">{{ $category->position }}</p>
-                    <p wire:sortable.handle style="width: 100%"><i class="fas fa-bars"></i></p>
-                    <p style="width: 100%; display: flex;">
-                        <form action="{{ route("categories.destroy", $category->id) }}" method="post">
-                            <a href="{{ route('categories.edit', $category->id) }}" class="mx-1 px-1 py-1 rounded"><i class="transition duration-200 ease-in-out text-blue-500 hover:text-blue-700 fas fa-edit"></i></a>
-                            @csrf
-                            @method('DELETE')
-                            <button class="mx-1 px-1 py-1 rounded outline-none" type="submit"><i class="transition duration-200 ease-in-out text-red-500 hover:text-red-700 fas fa-trash"></i></button>
-                        </form>
-                    </p>
+                <li wire:sortable.item="{{ $category->id }}" wire:key="task-{{ $category->id }}" class="item">
+                    <p>{{ $category->name }}</p>
+                    <p>{{ $category->position }}</p>
+                    <p wire:sortable.handle class="item-handle"><i class="fas fa-bars"></i></p>
+                    <form action="{{ route("categories.destroy", $category->id) }}" method="post">
+                        <a href="{{ route('categories.edit', $category->id) }}" class="mx-1 px-1 py-1 rounded"><i class="transition duration-200 ease-in-out text-blue-500 hover:text-blue-700 fas fa-edit"></i></a>
+                        @csrf
+                        @method('DELETE')
+                        <button class="mx-1 px-1 py-1 rounded outline-none" type="submit"><i class="transition duration-200 ease-in-out text-red-500 hover:text-red-700 fas fa-trash"></i></button>
+                    </form>
                 </li>
             @endforeach
         </ul>
